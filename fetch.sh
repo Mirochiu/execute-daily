f6458d46b53f609c77e28ec85d74ca4e9198e511
Yung-Hsiang Chiu <mirror.tw@gmail.com>
2023-09-04 09:41:32 +0800

    show status for fetch.sh

--- fetch.sh
+++ fetch.sh
@@ -2,8 +2,15 @@
 
 echo "fetch @ $(date +'%Y%m%d')"
 
-curl 'https://www.coolpc.com.tw/evaluate.php' > "coolpc-$(date +'%Y%m%d')_big5.html"
+echo "fetching coolpc..."
+curl --connect-timeout 10 'https://www.coolpc.com.tw/evaluate.php' > "coolpc-$(date +'%Y%m%d')_big5.html"
 iconv -f big5 -t utf-8 "coolpc-$(date +'%Y%m%d')_big5.html" > "coolpc-$(date +'%Y%m%d').html"
-curl 'https://nt66mobile.com.tw/' >  "nt66-$(date +'%Y%m%d').html"
-curl 'https://www.jyes.com.tw/product.php' > "jyes-$(date +'%Y%m%d').html"
-curl 'https://www.landtop.com.tw/product_categories/phones' > "landtop-$(date +'%Y%m%d').html"
+
+echo "fetching nt66..."
+curl --connect-timeout 10 'https://nt66mobile.com.tw/' >  "nt66-$(date +'%Y%m%d').html"
+
+echo "fetching jyes..."
+curl --connect-timeout 10 'https://www.jyes.com.tw/product.php' > "jyes-$(date +'%Y%m%d').html"
+
+echo "fetching landtop..."
+curl --connect-timeout 10 'https://www.landtop.com.tw/product_categories/phones' > "landtop-$(date +'%Y%m%d').html"
